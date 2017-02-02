ffb469b8e8bb359ef5166692068952c326f36848
Stefan Babic <babic.stefan@gmail.com>
2017-02-02 11:42:47 +0100

    finish password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root   'static_pages#home'
   get    'admin'   => 'admin#index'
 
@@ -22,5 +26,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
